1dc29ab50b6293b5275843291129251756b6b8e3
Stephen <stephen.stengel@cwu.edu>
2019-05-15 09:54:44 -0700

    Added a while loop.

--- InputPassword.java
+++ InputPassword.java
@@ -13,58 +13,34 @@ public class InputPassword {
         Scanner input = new Scanner(System.in);
         PasswordSecurity x = new PasswordSecurity();
 
-        System.out.print("What is your password? ");
-        String password = input.nextLine();
-
-        if (x.character(password)) {
-
-            System.out.println("Your password is the correct length");
-
-        } else {
-
-            System.out.println("Your password should be at least 6 character long");
-
+        boolean isPasswordGood = false;
+        
+        while (!isPasswordGood) {
+            System.out.print("\n\nEnter your password: ");
+            String password = input.nextLine();
+            System.out.println("");
+            
+            if (!x.character(password)) {
+				System.out.println("Your password is too short! Your password should be at least 6 character long");
+            }
+            else if (!x.lower(password)) {
+				System.out.println("The password needs to include at least one lowercase letter");
+            }
+            else if (!x.upper(password)) {
+				System.out.println("The password needs to include at least one uppercase letter");
+            }
+            else if (!x.digit(password)) {
+				System.out.println("The password must contain at least one digit");
+            }
+            
+            else if (!x.special(password)) {
+				System.out.println("The password should contain at least one special character");
+            }
+            else {
+				isPasswordGood = true;
+			}
         }
-
-        if (x.lower(password)) {
-
-            System.out.println("The password includes at least one lowercase letter");
-
-        } else {
-
-            System.out.println("The password needs to include at least one lowercase letter");
-
-        }
-
-        if (x.upper(password)) {
-
-            System.out.println("The password includes at least one uppercase letter");
-
-        } else {
-
-            System.out.println("The password needs to include at least one uppercase letter");
-
-        }
-
-        if (x.digit(password)) {
-
-            System.out.println("The password contains at least one digit");
-
-        } else {
-
-            System.out.println("The password must contain at least one digit");
-
-        }
-
-        if (x.special(password)) {
-
-            System.out.println("The password contains at least one special character");
-
-        } else {
-
-            System.out.println("The password should contain at least one special character");
-
-        }
-
+        
+        System.out.println("Yep, that's a good password!");
     }
 }
